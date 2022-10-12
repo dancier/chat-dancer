a72a7e2d27dfa66743b9f308d05ebf59907b06ec
Stelios Rusta <steliosrusta@gmail.com>
2022-10-12 22:46:02 +0200

    Add Java boilerplate

--- README.md
+++
@@ -1,2 +0,0 @@
-# chat-dancer
-Chat backend for dancer
---
+++ src/test/java/net/dancer/chatdancer/ChatDancerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dancer.chatdancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
