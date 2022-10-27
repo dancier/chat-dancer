f671609f58a8d68d71cef0220653266a2cb5c874
Marc Gorzala <marc@dancier.net>
2022-10-27 05:15:17 +0200

    added logging to ChatController

--- src/main/java/net/dancier/chatdancer/ChatController.java
+++ src/main/java/net/dancier/chatdancer/ChatController.java
@@ -2,6 +2,8 @@ package net.dancier.chatdancer;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ChatController {
 
+    public static Logger log = LoggerFactory.getLogger(ChatController.class);
     private final ChatService chatService;
     ModelMapper modelMapper = new ModelMapper();
     @GetMapping
@@ -23,6 +26,7 @@ public class ChatController {
     @PostMapping
     public ResponseEntity<ChatCreatedResponseDto> createNewChat (@RequestBody ChatCreatedResponseDto chatCreatedResponseDto) {
 
+        log.info("Creating Chat: " + chatCreatedResponseDto);
 
          Chat createdChat = chatService.createNewChat(chatCreatedResponseDto);
 
