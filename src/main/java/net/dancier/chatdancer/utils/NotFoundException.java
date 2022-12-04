16a1fad3ab032d0f2fbcc79106a29d0d54bf6e35
Stelios Rusta <steliosrusta@gmail.com>
2022-12-04 23:32:56 +0100

    Work in progress, Add some test and some error handling

---
+++ src/main/java/net/dancier/chatdancer/utils/NotFoundException.java
@@ -0,0 +1,8 @@
+package net.dancier.chatdancer.utils;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
