54baf26e22c66e0bf33cb85db335dc718fede7e6
Marc Gorzala <marc@dancier.net>
2023-11-05 17:02:29 +0100

    added liquibase

--- src/main/java/net/dancier/chatdancer/adapter/out/persistence/JpaChatEntity.java
+++ src/main/java/net/dancier/chatdancer/adapter/out/persistence/JpaChatEntity.java
@@ -1,4 +1,23 @@
 package net.dancier.chatdancer.adapter.out.persistence;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "chat")
+@NoArgsConstructor
 public class JpaChatEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
 }
